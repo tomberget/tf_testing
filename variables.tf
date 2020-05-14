1c5ae0a9573f897f01ce2cd17504c74b77c46482
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-14 15:22:18 +0200

    Added cluster name, put in more comments

--- variables.tf
+++ variables.tf
@@ -4,15 +4,24 @@ variable "aws_region" {
   default     = "eu-north-1"
 }
 
+# Path to tfstate in S3 bucket
 variable "s3_tfstate_key" {
   description = "Terraform state key (path)"
   default     = "terraform/terraform.tfstate"
 }
 
+## ENV variables
+# adding a prefix
 variable "prefix" {
   default = "atari"
 }
 
+# adding a environment
 variable "environment" {
   default = "dev"
+}
+
+# defining EKS cluster names
+variable "cluster-name" {
+  default = "atari-eks-cluster-dev"
 }
