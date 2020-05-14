d872150681c23222e727232b04dbdb913398d2f4
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-14 13:10:39 +0200

    Removed assume_role section

--- main.tf
+++ main.tf
@@ -1,42 +1,10 @@
-# create an assumed role
-resource "aws_iam_role" "assumed_role" {
-  name = "assumed_role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::remote-state"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject"],
-      "Resource": "arn:aws:s3:::${var.s3_tfstate_key}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem"
-      ],
-      "Resource": "arn:aws:dynamodb:*:*:dynamodb-terraform-state-lock/${var.prefix}-remote-state-lock-${var.environment}"
-    }
-  ]
-}
-POLICY
-}
-
 # create a bucket for remote state file
 resource "aws_s3_bucket" "bucket" {
-  bucket = "remote-state"
+  bucket = "${var.prefix}-remote-state"
   acl    = "private"
 
   tags = {
-    Name        = "remote-state"
+    Name        = "${var.prefix}-remote-state"
     Environment = var.environment
   }
 
