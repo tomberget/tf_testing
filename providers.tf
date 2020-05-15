b8ed3c1fa2b45edaa511343a63ba22597ff83bfb
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-15 15:31:28 +0200

    Added kubernetes and a few other providers

--- providers.tf
+++ providers.tf
@@ -3,8 +3,22 @@ provider "aws" {
   version = ">= 2.38.0"
 }
 
-# Using these data sources allows the configuration to be
-# generic for any region.
-data "aws_region" "current" {}
+provider "random" {
+  version = "~> 2.1"
+}
 
-data "aws_availability_zones" "available" {}
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
