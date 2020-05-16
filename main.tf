511105f564684ebc0c2c3eb3861a6b7cb0b692e7
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-16 02:06:01 +0200

    Introduced prevent_destroy on bucket and dynamodb

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = true
   }
+
+  # We explicitly prevent destruction using terraform. Remove this only if you really know what you're doing.
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # create a dynamodb table for locking the state file
@@ -28,6 +33,11 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   tags = {
     Name        = "${var.prefix}-remote-state-lock-${var.environment}"
     Environment = var.environment
+  }
+
+  # We explicitly prevent destruction using terraform. Remove this only if you really know what you're doing.
+  lifecycle {
+    prevent_destroy = true
   }
 
   depends_on = [aws_s3_bucket.bucket]
