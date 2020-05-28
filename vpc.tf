f4d38e8d3d34616083537635cbbcca9fdad7d68c
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-28 21:39:44 +0200

    Moved VPC out of EKS

---
+++ vpc.tf
@@ -0,0 +1,98 @@
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id 
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "public_0" {
+  vpc_id                  = aws_vpc.default.id 
+  cidr_block              = "10.0.101.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.default.id 
+  cidr_block              = "10.0.102.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "private_0" {
+  vpc_id                  = aws_vpc.default.id 
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.default.id 
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+# A security group for the ELB so it is accessible via the VM
+resource "aws_security_group" "elb" {
+  name        = "${var.prefix}_sec_group_elb_${var.environment}"
+  description = "ELB Security Group"
+  vpc_id      = aws_vpc.default.id 
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#resource "aws_elb" "web" {
+#  name = "take2-elb"
+
+#  subnets         = ["${aws_subnet.public_0.id}", "${aws_subnet.public_1.id}"]
+#  security_groups = ["${aws_security_group.elb.id}"]
+#  instances       = ["${aws_instance.frontend.id}"]
+
+#  listener {
+#    instance_port     = 80
+#    instance_protocol = "http"
+#    lb_port           = 80
+#    lb_protocol       = "http"
+#  }
+#}
