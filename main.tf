3131ff11c3104b86ee648420c9f5490243a2789a
Tom Stian Berget <tom.berget@tietoevry.com>
2020-05-28 21:39:25 +0200

    Added traefik and prometheus-operator

--- main.tf
+++ main.tf
@@ -3,13 +3,17 @@ module "monitoring" {
   source = "./modules/monitoring"
 }
 
-module "external_dns" {
-  source = "./modules/external_dns"
+#module "external_dns" {
+#  source = "./modules/external_dns"
 
-  cluster_id = module.eks.cluster_id
+#  cluster_id = module.eks.cluster_id
 
-  external_dns_id            = "aws"
-  external_dns_region        = var.aws_region
-  external_dns_chart_version = "2.22.1"
-  account_id                 = data.aws_caller_identity.current.account_id
+#  external_dns_id            = "aws"
+#  external_dns_region        = var.aws_region
+#  external_dns_chart_version = "2.22.1"
+#  account_id                 = data.aws_caller_identity.current.account_id
+#}
+
+module "traefik" {
+  source = "./modules/traefik"
 }
