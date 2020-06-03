80f087bcca5535e481e2de7a6257d9cc90ecd96c
Tom Stian Berget <tom.berget@tietoevry.com>
2020-06-03 15:52:33 +0200

    Fixed external dns and added nginx

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 
 module "monitoring" {
   source = "./modules/monitoring"
+  domain_name = var.domain_name
+  org_name = var.prefix
 }
 
-#module "external_dns" {
-#  source = "./modules/external_dns"
+module "external_dns" {
+  source                     = "./modules/external_dns"
+  cluster_id                 = module.eks.cluster_id
+  external_dns_id            = "aws"
+  external_dns_region        = var.aws_region
+  external_dns_chart_version = "2.22.1"
+  account_id                 = data.aws_caller_identity.current.account_id
+}
 
-#  cluster_id = module.eks.cluster_id
-
-#  external_dns_id            = "aws"
-#  external_dns_region        = var.aws_region
-#  external_dns_chart_version = "2.22.1"
-#  account_id                 = data.aws_caller_identity.current.account_id
-#}
-
-module "traefik" {
-  source = "./modules/traefik"
+module "nginx" {
+  source      = "./modules/nginx"
+  domain_name = var.domain_name
 }
