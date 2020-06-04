595b27ed4a60136821f9de1238a2f48e4b7ce867
Tom Stian Berget <tom.berget@tietoevry.com>
2020-06-04 21:20:11 +0200

    Correct the assume role principal arn

--- policy.tf
+++ policy.tf
@@ -61,6 +61,14 @@ resource "aws_iam_role" "external_dns" {
           "${replace(data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer, "https://", "")}:sub": "system:serviceaccount:external-dns:external-dns"
         }
       }
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${module.eks.worker_iam_role_arn}"
+      },
+      "Action": "sts:AssumeRole"
     }
   ]
 }
@@ -72,38 +80,4 @@ EOF
 resource "aws_iam_role_policy_attachment" "external_dns" {
   role       = aws_iam_role.external_dns.name
   policy_arn = aws_iam_policy.allow_external_dns_updates.arn
-}
-
-resource "aws_iam_role" "external_dns_update" {
-  name = var.external_dns_role_name
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${aws_iam_openid_connect_provider.default.arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${replace(data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer, "https://", "")}:sub": "system:serviceaccount:external-dns:external-dns"
-        }
-      }
-    },
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.external_dns_role_name}"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
-  depends_on = [aws_iam_role_policy_attachment.external_dns]
 }
