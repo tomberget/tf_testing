725d0d0b812d42a71f5170b38187537d0c7b0865
Tom Stian Berget <tom.berget@tietoevry.com>
2020-06-05 11:48:25 +0200

    Added kubernetes-cluster dashboard from Grafana Dashboards

--- modules/monitoring/prometheus-operator.tf
+++ modules/monitoring/prometheus-operator.tf
@@ -32,3 +32,18 @@ data "template_file" "prometheus_operator_config" {
     prometheus_tls_secret_name   = "prometheus-${replace("${var.domain_name}", ".", "-")}-tls"
   }
 }
+
+resource "kubernetes_config_map" "grafana_dashboards" {
+  metadata {
+    name      = "grafana-dashboards"
+    namespace = kubernetes_namespace.prometheus.metadata[0].name
+
+    labels = {
+      grafana_dashboard = "1"
+    }
+  }
+
+  data = {
+    "kubernetes-cluster_rev1.json" = file("${path.root}/config/grafana/dashboards/kubernetes-cluster_rev1.json")
+  }
+}
